3a5a39698e19710f8293035d9dc5f4bba050f772
Tareq0Hasan <tareq1363@gmail.com>
2024-04-30 11:40:19 +0600

    first commit

---
+++ src/main/java/com/example/ovi/JgtdslExternalApplication.java
@@ -0,0 +1,14 @@
+package com.example.ovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgtdslExternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgtdslExternalApplication.class, args);
+		System.out.println("connected");
+	}
+
+}
